2f485d0c844b20fa3f7a49529282ea467cc7044a
Alex <autyan@outlook.com>
2016-08-17 22:00:53 +0800

    完善接口注释

--- SHWDTech.RepositoryModel/IRepositoryModelBase.cs
+++ SHWDTech.RepositoryModel/IRepositoryModelBase.cs
@@ -2,10 +2,19 @@
 {
     public interface IRepositoryModelBase
     {
+        /// <summary>
+        /// 是否是新增数据（未持久化）
+        /// </summary>
         bool IsNew { get; }
 
+        /// <summary>
+        /// 是否标记为删除
+        /// </summary>
         bool IsDeleted { get; set; }
 
+        /// <summary>
+        /// 是否为启用状态
+        /// </summary>
         bool IsEnabled { get; set; }
     }
 }
